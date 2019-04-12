ebb91bee6485a960193d9fa3473130de2730eabd
Claisa Lubke <claisa.lubke@nsinova.com.br>
2019-04-12 17:32:20 -0300

    Change bullet speed

--- src/Bullet.js
+++ src/Bullet.js
@@ -4,19 +4,20 @@ export class Bullet {
     this.position = position
     this.speed = {
       x: 0,
-      y: 50
+      y: -300
     }
     this.color = color
   }
 
   update (delta, context) {
     this.position.x = parseInt(this.position.x + (this.speed.x / 60))
-    this.position.y = parseInt(this.position.y - (this.speed.y / 60))
+    this.position.y = parseInt(this.position.y + (this.speed.y / 60))
     this.draw(context)
   }
 
   draw (context) {
-    context.rect(this.position.x - 4, this.position.y, 8, 8)
+    context.clearRect(0, 0, 8000, 8000)
+    context.rect(this.position.x - 4, this.position.y, 8, 1)
     context.fillStyle = this.color
     context.fill()
   }
